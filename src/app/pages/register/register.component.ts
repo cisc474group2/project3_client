3d4a52a535c5805e556ac07ae1306776826834f0
Timothy Mazzarelli <tmazzar@udel.edu>
2020-11-15 13:19:12 -0500

    decent looking register page

--- src/app/pages/register/register.component.ts
+++ src/app/pages/register/register.component.ts
@@ -11,6 +11,10 @@ import { AuthService } from 'src/app/services/auth.service';
 export class RegisterComponent implements OnInit {
   
   hidden = false;
+  hideIfBusiness = true;
+  hideIfIndividual = true;
+  showIfBusiness = false;
+  showIfIndividual = false;
   registerForm: FormGroup;
   loading =false;
   submitted=false;
@@ -28,8 +32,16 @@ export class RegisterComponent implements OnInit {
     this.returnUrl=this.route.snapshot.queryParams['returnUrl'] || '/';
   }
 
-  unhide(){
+  unhideForIndividual(){
     this.hidden = !this.hidden;
+    this.hideIfIndividual = !this.hideIfIndividual;
+    this.showIfIndividual = !this.showIfIndividual;
+  }
+
+  unhideForBusiness(){
+    this.hidden = !this.hidden;
+    this.hideIfBusiness = !this.hideIfBusiness;
+    this.showIfBusiness = !this.showIfBusiness;
   }
 
   register(){
