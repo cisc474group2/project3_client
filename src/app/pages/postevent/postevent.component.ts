e0593c887bb02a1b5c9d8b9a5aa7d6f663941747
Sarah Joyce <sjoyce@udel.edu>
2020-11-18 16:17:15 -0500

    events form updates, not working yet

--- src/app/pages/postevent/postevent.component.ts
+++ src/app/pages/postevent/postevent.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { FormGroup, FormBuilder, Validators } from '@angular/forms';
 import { Event, Geoloc } from '../../../assets/model';
 import { ActivatedRoute, Router } from '@angular/router';
+import { AuthService } from 'src/app/services/auth.service';
 
 @Component({
   selector: 'app-postevent',
@@ -11,16 +12,22 @@ import { ActivatedRoute, Router } from '@angular/router';
 export class PosteventComponent implements OnInit {
 
   submitted=false;
+  loading =false;
   returnUrl: string;
   eventsForm: FormGroup;
+  error: string;
+  event: Event;
 
-  constructor(private formBuilder: FormBuilder, private route: ActivatedRoute) { }
+  constructor(private formBuilder: FormBuilder, private route: ActivatedRoute, private router: Router) { }
 
   ngOnInit(): void {
     this.eventsForm=this.formBuilder.group({
       title: '',
       description: '',
-      event_address: '',
+      eventStreet: '',
+      eventCity: '',
+      eventState: '',
+      eventZip: '',
       start_time: '',
       end_time: ''
     });
@@ -33,13 +40,26 @@ export class PosteventComponent implements OnInit {
     if (this.eventsForm.invalid) {
       return;
     }
-    this.eventsForm.controls.title.value,
-    this.eventsForm.controls.description.value,
-    new Geoloc(this.eventsForm.controls.eventStreet.value + "+" 
-    + this.eventsForm.controls.eventCity.value + "+"
-    + this.eventsForm.controls.eventState.value
-    //+ "+" + this.eventsForm.controls.eventZip.value
-    ),
-    this.eventsForm.controls.eventStreet.value
+    this.loading = true;
+      this.event = new Event(
+        this.eventsForm.controls.title.value,
+        this.eventsForm.controls.description.value,
+        new Geoloc(this.eventsForm.controls.eventStreet.value + "+" 
+        + this.eventsForm.controls.eventCity.value + "+"
+        + this.eventsForm.controls.eventState.value
+        //+ "+" + this.eventsForm.controls.eventZip.value
+        ),
+        this.eventsForm.controls.start_time.value,
+        this.eventsForm.controls.end_time.value
+      );
+    
+    
+
+    this.authSvc.postEvent(
+      this.event)
+      .subscribe(response=>{
+        this.router.navigate([this.returnUrl]);
+      },err=>{this.submitted=false;this.loading=false;this.error=err.message||err;});
   }
+ 
 }
--- src/app/services/events.service.ts
+++ src/app/services/events.service.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@angular/core';
-import { Observable } from 'rxjs';
+import { Observable, ReplaySubject, throwError } from 'rxjs';
 import { HttpClient } from '@angular/common/http';
+import { map, catchError } from 'rxjs/operators';
 
 @Injectable({
   providedIn: 'root'
@@ -8,9 +9,23 @@ import { HttpClient } from '@angular/common/http';
 export class EventsService {
 
   private path="http://localhost:3000/api/"
-  constructor(private http:HttpClient) { }
+
+  constructor(private http:HttpClient) {
+  }
 
   getEvents(): Observable<any>{
     return this.http.get(this.path+'events');
+  }
+
+  postEvent(title: string, description: string, event_address: string, start_time: string, end_time: string): Observable<any>{
+    return this.http.post<any>(this.path,{ title: title, description: description, event_address: event_address, start_time: start_time, end_time: end_time})
+      .pipe(map(event=>{
+        title=event.data.title
+        description=event.data.description
+        event_address=event.data.event_address
+        start_time=event.data.start_time
+        end_time=event.data.end_time
+        return event.data;
+      }),catchError(err=>{return throwError(err.message||'server error')}));
   }
 }
