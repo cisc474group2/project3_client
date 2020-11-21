2faedb6d2b473871289826627631438946927c71
Timothy Mazzarelli <tmazzar@udel.edu>
2020-11-21 18:50:07 -0500

    adding outline for profile page

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,13 +4,15 @@ import { HomeComponent } from './pages/home/home.component';
 import { LoginComponent } from './pages/login/login.component';
 import { RegisterComponent } from './pages/register/register.component';
 import { PosteventComponent } from './pages/postevent/postevent.component';
+import {ProfileComponent} from '.pages/profile/profile.component'
 
 const routes: Routes = [
   {path:'',redirectTo:'home',pathMatch:'full'},
   {path:'home',component: HomeComponent},
   {path:'login',component: LoginComponent},
   {path:'register',component: RegisterComponent},
-  {path:'postevent',component: PosteventComponent}
+  {path:'postevent',component: PosteventComponent},
+  {path:'profile', component: ProfileComponent}
 ];
 
 @NgModule({
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -11,6 +11,7 @@ import { HttpClientModule, HTTP_INTERCEPTORS } from '@angular/common/http';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { TokenInterceptorService } from './services/token-interceptor.service';
 import { PosteventComponent } from './pages/postevent/postevent.component';
+import {ProfileComponent} from '.pages/profile/profile.component'
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { fromEventPattern } from 'rxjs';
 
@@ -20,7 +21,8 @@ import { fromEventPattern } from 'rxjs';
     HomeComponent,
     LoginComponent,
     RegisterComponent,
-    PosteventComponent
+    PosteventComponent,
+    ProfileComponent
   ],
   imports: [
     BrowserModule,
