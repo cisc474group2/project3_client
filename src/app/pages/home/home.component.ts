eab4e2505464759efef4a05c71c1363d502b5e06
Sarah Joyce <sjoyce@udel.edu>
2020-11-30 14:46:46 -0500

    register button updates

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -15,6 +15,12 @@ export class HomeComponent implements OnInit {
   i = 0;
   constructor(private eventSvc:EventsService, private profileSvc:ProfileService, private authSvc:AuthService) { 
     this.g = eventSvc.getEventsFormat();
+    this.g.forEach(element => {
+      if(!this.authSvc.userObject.reg_events.includes(element._id)){
+        document.getElementById('card._id').classList.remove('btn-primary');
+        document.getElementById('card._id').classList.add('btn-secondary');
+      }
+    });
   }
 
   ngOnInit(): void {
@@ -22,15 +28,22 @@ export class HomeComponent implements OnInit {
 
   registerUser(event_id){
     this.authSvc.authorize();
-    this.authSvc.userObject.reg_events.push(event_id);
-    this.profileSvc.updateUser(this.authSvc.userObject._id, this.authSvc.userObject.email, 
-      this.authSvc.userObject.type_obj, this.authSvc.userObject.reg_events).subscribe(response=>{
-        console.log(response);
-      },err=>{console.error(err);});
-    
-      this.eventSvc.updateUserList(event_id, this.authSvc.userObject._id).subscribe(response=>{
-        console.log(response);
-      },err=>{console.error(err);});
+    if(!this.authSvc.userObject.reg_events.includes(event_id)){
+      this.authSvc.userObject.reg_events.push(event_id);
+
+      this.profileSvc.updateUser(this.authSvc.userObject._id, this.authSvc.userObject.email, 
+        this.authSvc.userObject.type_obj, this.authSvc.userObject.reg_events).subscribe(response=>{
+          console.log(response);
+        },err=>{console.error(err);});
+      
+        this.eventSvc.updateUserList(event_id, this.authSvc.userObject._id).subscribe(response=>{
+          console.log(response);
+        },err=>{console.error(err);});
+
+        document.getElementById('').innerHTML='Unregister';
+        document.getElementById('card._id').classList.remove('btn-primary');
+        document.getElementById('card._id').classList.add('btn-secondary');
     }
+  }
 
 }
